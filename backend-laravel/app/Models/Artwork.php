0c1d6ae5c918154d2fdd3095dfc7fb04899b8c0c
Albert <albert.puertas@cirvianum.cat>
2025-12-28 12:34:59 +0000

    Creació model artwork i soft delete users

---
+++ backend-laravel/app/Models/Artwork.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+class Artwork extends Model
+{
+    use HasFactory, Notifiable, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'price',
+        'user_id'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
